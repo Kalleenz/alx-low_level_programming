1de0b560609089b1831adf538333d2671eef137a
Kelechiswe <kalleenz070@gmail.com>
2023-07-13 18:34:44 -0700

    quotes1

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
 
 /**
- * main - This makes use of fputs
+ * main - This used the fputs function
+ *
  * Return: 1
  */
 int main(void)
 {
-	fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
+	fputs("and that piece of art is useful\" - Dora Korpar, 2015-10-19", stdout);
 	return (1);
 }
