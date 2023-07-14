7f4c5a0fbbe17b22e0dee249d91fe4da6fd738d6
Kelechiswe <kalleenz070@gmail.com>
2023-07-14 15:11:18 -0700

    upper and lowercase

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description - Getting upper and lower case alphabets
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char let;
+
+	for (let = 'A; let <= Z; let++)
+	{
+		putchar(let);
+	}
+	for (let = 'a'; let <= 'z'; let++)
+	{
+		putchar(let);
+	}
+	putchar('\n');
+	return 0;
+}
