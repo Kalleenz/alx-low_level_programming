859f3433df4dbe251a351032f7f004e6c8b297d6
Kelechiswe <kalleenz070@gmail.com>
2023-07-14 16:31:09 -0700

    base16

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Entry mode
+ * description - Printing in base 16
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int x;
+
+	for (x = 0; x <= 9; x++)
+	{
+		putchar(x + '0');
+	}
+	for (x = a; x <= f; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
