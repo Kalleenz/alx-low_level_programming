a67f41edbce70b9b7a5bdd052730ebb1573ae986
Kelechiswe <kalleenz070@gmail.com>
2023-07-14 15:20:59 -0700

    upperandlowcases

--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -8,11 +8,11 @@ int main(void)
 {
 	char let;
 
-	for (let = 'A'; let <= 'Z'; let++)
+	for (let = 'a'; let <= 'z'; let++)
 	{
 		putchar(let);
 	}
-	for (let = 'a'; let <= 'z'; let++)
+	for (let = 'A'; let <= 'Z'; let++)
 	{
 		putchar(let);
 	}
