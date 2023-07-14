9e34b3e1d503a7b36f4cddc0b62704c5b8131595
Kelechiswe <kalleenz070@gmail.com>
2023-07-14 15:43:22 -0700

    numbers

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description - printing base 10
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num < 10; num++)
+	{
+		putchar(num);
+	}
+	putchar('\n');
+	return (0);
+}
