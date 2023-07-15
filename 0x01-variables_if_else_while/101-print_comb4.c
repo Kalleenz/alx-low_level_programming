5523362b705a188ceb196e329152de7f7d7f68e5
Kelechiswe <kalleenz070@gmail.com>
2023-07-14 17:44:49 -0700

    combo

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry mode
+ * description - Printing double digit that are different
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int f;
+	int s;
+
+	for (f = 0; f <= 8; f++)
+	{
+		for (s = f + 1; s <= 9; s++)
+		{
+			putchar(f + '0');
+			putchar(s + '0');
+
+			if (f < 8 || s < 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
