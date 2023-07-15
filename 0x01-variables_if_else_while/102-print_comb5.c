3e7b397c6dbab96bbee181abb9ea53d9549c4882
Kelechiswe <kalleenz070@gmail.com>
2023-07-14 19:19:45 -0700

    103combo

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * description - printing all possible combos
+ * Return: 0 Success
+ */
+int main(void)
+{
+	int f;
+	int s;
+
+	for (f = 0; f <= 99; f++)
+	{
+		for (s = 1; s <= 99; s++)
+		{
+			int num1 = f / 10;
+			int num2 = f % 10;
+			int num3 = s / 10;
+			int num4 = s % 10;
+
+			putchar(num1 + '0');
+			putchar(num2 + '0');
+			putchar(' ');
+			putchar(num3 + '0');
+			putchar(num4 + '0');
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
