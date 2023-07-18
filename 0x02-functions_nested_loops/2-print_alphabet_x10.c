690ef52a56dc7e1b1eeece11178b4f9140a00b35
Kelechiswe <kalleenz070@gmail.com>
2023-07-18 13:40:47 -0700

    x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - printing alphabets times 10
+ */
+void print_alphabet_x10(void)
+{
+	int i, v;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (v = 'a'; v <= 'z'; v++)
+		{
+			_putchar(v);
+		}
+	}
+	_putchar('\n');
+}
