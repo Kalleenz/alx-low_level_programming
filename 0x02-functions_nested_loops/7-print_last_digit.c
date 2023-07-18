16ab829f28404eeb64e600a053ddf6400c786401
Kelechiswe <kalleenz070@gmail.com>
2023-07-18 16:15:15 -0700

    lastdigit

---
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_last_digit - printing last digits
+ * @d: integer
+ * Return: ld
+ */
+
+int print_last_digit(int d)
+{
+	int ld;
+
+	ld = d % 10
+	if (ld < 0)
+	{
+		ld = -ld
+	}
+	_putchar(ld + '0');
+	return (ld);
+}
