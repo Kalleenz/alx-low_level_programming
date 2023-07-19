47b7d4a4a9723d1dc179ac9fb045097424e9827c
Kelechiswe <kalleenz070@gmail.com>
2023-07-19 15:05:23 -0700

    fibonaccisum

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+
+/**
+ * main - printing the sum of even valued fibonacci numbers less tha 4000000
+ * Return: 0
+ */
+
+int main(void)
+{
+	int x;
+	long a = 1;
+	long b = 2;
+	long add = b;
+
+	for (x = 0; x++;)
+	{
+		while (b + a < 4000000)
+		{
+			b = b + a;
+			if (b % 2 == 0)
+			{
+				add = add + b;
+			}
+			a = b - a;
+		}
+		printf("%ld", add);
+	}
+	printf("\n");
+	return (0);
+}
