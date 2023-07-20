1a30797d59eda242cb8c6897f1a53f03cdd57cb3
Kelechiswe <kalleenz070@gmail.com>
2023-07-20 12:53:59 -0700

    prints

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * print_numbers - print all number 0 to 9
+ * Return: 0
+ */
+
+void print_numbers(void)
+{
+	int m;
+
+	for (m = 48; m <= 57; m++)
+	{
+		_putchar(m);
+	}
+	_putchar('\n');
+	return (0);
+}
