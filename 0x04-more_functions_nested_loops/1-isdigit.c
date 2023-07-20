a986ce6d0d38cb380cf4a5da924962f8361e7409
Kelechiswe <kalleenz070@gmail.com>
2023-07-20 12:17:14 -0700

    digitss

--- 0x04-more_functions_nested_loops/1-isdigit.c
+++ 0x04-more_functions_nested_loops/1-isdigit.c
@@ -3,12 +3,12 @@
 /**
  * _isdigit - checking for a digit through 0 and 9
  * @c: integer needed
- * Return: 1 or 0
+ * Return: 0 or 1
  */
 
 int _isdigit(int c)
 {
-	if (c >= 0 && c <= 9)
+	if (c >= 48 && c <= 57)
 		return (1);
 	else
 		return (0);
