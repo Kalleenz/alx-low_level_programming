d44646125ae24f49a62531ccc7a0f9a2bd61f137
Kelechiswe <kalleenz070@gmail.com>
2023-07-20 20:04:43 -0700

    triangle

---
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_triangle - printing triangle with #
+ * @size: number of triangle
+ * Return: void
+ */
+
+void print_triangle(int size)
+{
+	if (size <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		int m, n;
+
+		for (m = 0; m < size; m++)
+		{
+			for (n = m; n < size; n++)
+			{
+				_putchar(' ');
+			}
+			for (n = 0; n < m; n++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+			}
+		}
+	}
+}
