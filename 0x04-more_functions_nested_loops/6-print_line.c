a4c91c19d0750bd31786f93d8afb7b5e54039d3c
Kelechiswe <kalleenz070@gmail.com>
2023-07-20 17:51:41 -0700

    line1

--- 0x04-more_functions_nested_loops/6-print_line.c
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -8,17 +8,19 @@
 
 void print_line(int n)
 {
-	int m;
-
 	if (n > 0)
 	{
+		int m;
+
 		for (m = 1; m <= n; m++)
 		{
-			_putchar(95);
+			_putchar('_');
 		}
+		_putchar('\n');
 	}
 	else if (n <= 0)
 	{
 		_putchar('\n');
 	}
+
 }
