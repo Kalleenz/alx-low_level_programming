b88f98ff218ff0167b54751c4cdd3200f3ac053e
Kelechiswe <kalleenz070@gmail.com>
2023-07-24 17:35:35 -0700

    halfputs

---
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,29 @@
+#include "main.h"
+
+/**
+ * puts_half - prints the second half of  string
+ * @str: string needed
+ * Return: half string
+ */
+
+void puts_half(char *str)
+{
+	int l = 0;
+	int m;
+	int n;
+
+	for (m = 0; str[m] != '\0'; m++)
+	{
+		l++;
+	}
+	n = (l / 2);
+	if ((l % 2) == 1)
+	{
+		n = ((l + 1) / 2);
+	}
+	for (m = n; str[m] != '\0'; m++)
+	{
+		_putchar(str[m]);
+	}
+	_putchar('\n');
+}
