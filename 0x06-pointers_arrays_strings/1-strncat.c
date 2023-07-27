c9a6a9f1ea7eb7a645b390ad8f324069e62a08b5
Kelechiswe <kalleenz070@gmail.com>
2023-07-27 15:32:47 -0700

    strncat

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * _strncat - concatenates with a specific number of string
+ * @dest: destination
+ * @src: source
+ * @n: number of string
+ * Return: dest
+ */
+
+char *_strncat(char *dest, char *src, int n)
+{
+	int a;
+	int b;
+
+	for (a = 0; dest[a] != '\0'; a++)
+	{
+	}
+	for (b = 0; src[b] <=  n; b++)
+	{
+		dest[a] = src[b];
+		a++;
+	}
+	dest[a] = '\0';
+	return (dest);
+}
