b4746b8122f869014ff5ad475b5908aa955df211
Kelechiswe <kalleenz070@gmail.com>
2023-07-31 08:03:28 -0700

    strchr

---
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * _strchr - search for the first given character in a string
+ * @s: the string
+ * @c: character to be searched
+ * Return: s
+ */
+
+char *_strchr(char *s, char c)
+{
+	for (; s != '\0'; s++)
+	{
+		if (s == c)
+		{
+			return (s);
+		}
+	}
+	return (NULL);
+}
