de15164afca3eb050ea4d008cd2c851d94a09a36
Kelechiswe <kalleenz070@gmail.com>
2023-08-02 13:36:15 -0700

    putsrecursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _puts_recursion - entry point to print a string
+ * @s: string pointer
+ */
+
+void _puts_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		return;
+	}
+	_putchar(*s);
+	s++;
+	_puts_recursion(s);
+}
---
+++ 0x08-recursion/README.md
@@ -0,0 +1,1 @@
+This readme is basically all you need to know aboout recursions in c.
