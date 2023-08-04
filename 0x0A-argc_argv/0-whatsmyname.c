45f2ac92b295454c2ce837bd7f57cdf3c8fd34d7
Kelechiswe <kalleenz070@gmail.com>
2023-08-04 12:50:12 -0700

    whatsmyname

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @x: argument count
+ * @y: argument vector
+ * Return: 0
+ */
+
+int main(int x, char *y[])
+{
+	(void)x;
+	printf("%s\n",y[0]);
+
+	return (0);
+}
---
+++ 0x0A-argc_argv/README.md
@@ -0,0 +1,1 @@
+A readme to study the essence and importance of argument count (argc) and argument vector (argv).
