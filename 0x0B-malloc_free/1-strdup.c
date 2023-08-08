75d60a6eab17bc8c70e3481e87bb08162ea28595
Kelechiswe <kalleenz070@gmail.com>
2023-08-08 15:22:01 -0700

    copystring

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+
+/**
+ * _strdup - main entry to copy a string to a new allocated memory
+ * @str: string to be copied
+ * Return: a pointer
+ */
+
+char *_strdup(char *str)
+{
+	char *ptrdup;
+	int i, j;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+	if (str[i] == NULL)
+	{
+		return (NULL);
+	}
+
+	ptrdup = malloc((i * sizeof(char)) + 1);
+
+	if (ptrdup == NULL)
+	{
+		return (NULL);
+	}
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		ptrdup[j] = str[j];
+	}
+	ptrdup[i] = '\0';
+	return (ptrdup);
+}
