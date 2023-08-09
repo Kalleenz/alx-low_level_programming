854f5b887fe29545f26b583285228dae69329fcb
Kelechiswe <kalleenz070@gmail.com>
2023-08-09 09:48:37 -0700

    freegrid

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,20 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - freeing 2D grid
+ * @grid: 2D grid
+ * @height: row
+ * Return: nothing
+ */
+
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	for (i = 0; i < height; i++)
+	{
+		free(ptrgrip[i]);
+	}
+	free(ptrgrip);
+}
