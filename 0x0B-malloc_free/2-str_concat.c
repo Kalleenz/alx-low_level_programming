50243694a6458ab0021cc2ddd273f73f3b80bb44
Kelechiswe <kalleenz070@gmail.com>
2023-08-09 05:31:18 -0700

    strconcat4

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -14,9 +14,13 @@ char *str_concat(char *s1, char *s2)
 	char *ptrconcat;
 	int i, j, k, l;
 
-	if ((s1 == NULL || s2 == NULL) || (s1 == NULL && s2 == NULL))
+	if (s1 == NULL)
 	{
-		return ("");
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
 	}
 
 	for (i = 0; s1[i] != '\0'; i++)
