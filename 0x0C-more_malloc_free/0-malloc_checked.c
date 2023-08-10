45a9ee00c2451e4f62a27b7d5ac0137afa604f0b
Kelechiswe <kalleenz070@gmail.com>
2023-08-10 04:21:08 -0700

    mallocchecked

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * malloc_checked - entry point for memory allocation
+ * @b: integer
+ * Return: a pointer
+ */
+
+void *malloc_checked(unsigned int b)
+{
+	void *ptr;
+
+	ptr = malloc(sizeof(void *) * b);
+
+	if (ptr == NULL)
+	{
+		exit(98);
+	}
+	return (ptr);
+}
---
+++ 0x0C-more_malloc_free/README.md
@@ -0,0 +1,1 @@
+This readme tells us more about malloc and free like the exit function.
