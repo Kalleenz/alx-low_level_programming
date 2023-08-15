3739173534098d0ba01ace876637c33e5568d385
Kelechiswe <kalleenz070@gmail.com>
2023-08-15 08:14:09 -0700

    doginit5

--- 0x0E-structures_typedef/1-init_dog.c
+++ 0x0E-structures_typedef/1-init_dog.c
@@ -8,16 +8,14 @@
  * @name: name of dog
  * @age: age of dog
  * @owner: owner of the dog
- * Return: user
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
 	if (d == NULL)
-	{
 		d = malloc(sizeof(struct dog));
-		(*d).name = name;
-		d->age = age;
-		d->owner = owner;
-	}
+
+	d->name = name;
+	d->age = age;
+	d->owner = owner;
 }
