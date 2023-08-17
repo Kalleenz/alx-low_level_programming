4234580f2b0b2aaf0a0bf1f9c483b900a3af59bc
Kelechiswe <kalleenz070@gmail.com>
2023-08-17 06:13:24 -0700

    sumthem

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - sum all the given integers
+ * @n: number of arguments
+ * Return: an integer
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list su;
+
+	unsigned int i;
+	unsigned int sum = 0;
+
+	if (n == 0)
+	{
+		return (0);
+	}
+	va_start(su, n);
+
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(su, int);
+	}
+	va_end(su);
+	return (sum);
+}
---
+++ 0x10-variadic_functions/README.md
@@ -0,0 +1,1 @@
+This readme explains the importance of variadic functions and the use of their macros from stdarg.h header file.
