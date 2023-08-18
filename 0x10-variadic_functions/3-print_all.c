2846597e62f373668e3c175e7aa0a5658bcc61ab
Kelechiswe <kalleenz070@gmail.com>
2023-08-18 08:55:55 -0700

    all

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,46 @@
+#include "variadic_functions.h"
+#include <stdio.h>
+/**
+ * print_all - printing all types of data in the format list
+ * @format: list of types
+ */
+void print_all(const char * const format, ...)
+{
+	va_list all;
+	int a = 0;
+	char *delimiter = "";
+	char *string;
+
+	va_start(all, format);
+	if (format != NULL)
+	{
+		while (format[i] != '\0')
+		{
+			switch (format[i])
+			{
+				case 'c':
+					printf("%s%c", delimiter, va_arg(all, int));
+					break;
+				case 'i':
+					printf("%s%d", delimiter, va_arg(all, int));
+					break;
+				case 'f':
+					printf("%s%f", delimiter, va_arg(all, double));
+					break;
+				case 's':
+					string = va_arg(all, char *);
+					if (string == NULL)
+						printf("(nil)");
+					printf("%s%s", delimiter, string);
+					break;
+				default:
+					a++;
+					continue;
+			}
+			delimiter = ", ";
+			a++;
+		}
+	}
+	printf("\n");
+	va_end(all);
+}
