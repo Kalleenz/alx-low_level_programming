8273916fad0add5afb815204d21d0ad8bf4cca37
Kelechiswe <kalleenz070@gmail.com>
2023-08-24 12:00:41 -0700

    addtobeginning

---
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+#include <stddef.h>
+#include <stdlib.h>
+#include "lists.h"
+
+/**
+ * add_node - adding to the beginning of a linked list
+ * @head: pointer to the pointer of the first node
+ * @str: string pointer
+ * Return: address of the new node
+ */
+list_t *add_node(list_t **head, const char *str)
+{
+	list_t *addptr;
+
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+	addptr = malloc(sizeof(list_t));
+	if (addptr == NULL)
+	{
+		return (NULL);
+	}
+	addptr->str = strdup(str);
+	if (addptr->str == NULL)
+	{
+		free(addptr);
+		return (NULL);
+	}
+	addptr->len = strlen(str);
+	addptr->next = *head;
+	*head = addptr;
+
+	return (addptr);
+}
