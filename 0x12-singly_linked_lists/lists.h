99f79d015f9938e08e296c0d23a954a0fb2e507e
Kelechiswe <kalleenz070@gmail.com>
2023-08-24 10:48:26 -0700

    listprint

--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,20 @@
 #ifndef LISTS_H
 #define LISTS_H
-#include <stddef.h>
+
+/**
+ * struct list_s - singly linked list
+ * @str: string - (malloc'ed string)
+ * @len: length of the string
+ * @next: points to the next node
+ *
+ * Description: singly linked list node structure
+ */
+typedef struct list_s
+{
+       	char *str;
+	unsigned int len;
+	struct list_s *next;
+} list_t;
 
 int _putchar(char c);
 
@@ -14,18 +28,4 @@ list_t *add_node_end(list_t **head, const char *str);
 
 void free_list(list_t *head);
 
-/**
- * struct list_s - singly linked list
- * @str: string - (malloc'ed string)
- * @len: length of the string
- * @next: points to the next node
- *
- * Description: singly linked list node structure
- */
-typedef struct list_s
-{
-	char *str;
-	unsigned int len;
-	struct list_s *next;
-} list_t;
 #endif
