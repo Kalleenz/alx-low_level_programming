3fb60803055acdecb35fd057fab5d16aac9909e6
Kelechiswe <kalleenz070@gmail.com>
2023-08-25 09:25:07 -0700

    first1

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -5,7 +5,9 @@
  * @void: no parameters
  */
 
-void before(void) __attribute__ ((constructor))
+void before(void) __attribute__ ((constructor));
+
+void before(void)
 {
 	printf("You're beat! and yet, you must allow,\n
 			I bore my house upon my back!\n");
