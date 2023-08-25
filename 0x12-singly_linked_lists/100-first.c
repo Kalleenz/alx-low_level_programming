7b112bd5ed24167adcb0ecf119987fcfa9759ddb
Kelechiswe <kalleenz070@gmail.com>
2023-08-25 09:49:29 -0700

    first4

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -4,9 +4,7 @@
  * before - printing a string before main is executed
  */
 
-void before(void) __attribute__ ((constructor))
-
-void before(void)
+void __attribute__((constructor)) before()
 {
 	printf("You're beat! and yet, you must allow,\n"
 	"I bore my house upon my back!\n");
