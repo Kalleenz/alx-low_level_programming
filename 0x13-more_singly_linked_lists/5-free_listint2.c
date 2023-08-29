591cf2eacae2af879988dff418f35cb0b0da7a1a
Kelechiswe <kalleenz070@gmail.com>
2023-08-29 15:05:03 -0700

    frr

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -13,14 +13,13 @@ void free_listint2(listint_t **head)
 
 	if (*head == NULL)
 	{
-		printf("(nil)");
 		return;
 	}
 
 	while (*head != NULL)
 	{
 		trav = (*head)->next;
-		free(trav);
+		free(*head);
 		*head = trav;
 
 	}
