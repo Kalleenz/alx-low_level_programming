176b3ce2c7ecee208bbd75a7e92f681b4937ce65
Kelechiswe <kalleenz070@gmail.com>
2023-08-29 12:15:59 -0700

    nth

--- 0x13-more_singly_linked_lists/7-get_nodeint.c
+++ 0x13-more_singly_linked_lists/7-get_nodeint.c
@@ -5,7 +5,21 @@
 /**
  * get_nodeint_at_index - inserting node at a particular position
  * @head: pointer to first node
- * @index: number of nodes starting from 0
+ * @index: the eaxct number of node starting from 0
  * Return: the node position
  */
 listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+{
+	unsigned int i;
+	listint_t *trav;
+
+	if (head == NULL)
+		return (NULL);
+
+	for (i = 0; i < index; i++)
+	{
+		trav = head;
+		trav = trav->next;
+	}
+	return (trav);
+}
