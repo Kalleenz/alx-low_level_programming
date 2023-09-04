59dfb246b5f07c523feecae514a7c285eb194469
Kelechiswe <kalleenz070@gmail.com>
2023-09-04 16:54:57 -0700

    read1

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -24,7 +24,7 @@ ssize_t read_textfile(const char *filename, size_t letters)
 		return (NULL);
 
 	r = read(f, let, letters);
-	w = write(STDOUT_FILENO, let, t);
+	w = write(STDOUT_FILENO, let, r);
 	free(let);
 	close(f);
 	return (w);
