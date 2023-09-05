6d6cb4bd236b7c5a67b5302dd1f41fee8b44b304
Kelechiswe <kalleenz070@gmail.com>
2023-09-05 00:42:51 -0700

    create1

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -20,7 +20,7 @@ int create_file(const char *filename, char *text_content)
 	for (l = 0; text_content[l] != '\0'; l++)
 	{
 	}
-	f = open(filename, O_CREAT | O_RDWR | O_TRUNC, S_IRUSR | S_IWUSR);
+	f = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (f == -1)
 		return (-1);
 	w = write(f, text_content, l);
