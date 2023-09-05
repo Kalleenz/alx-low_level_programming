29e233baa17b28df9658d1aa93824bb4a31a2c8a
Kelechiswe <kalleenz070@gmail.com>
2023-09-05 00:57:20 -0700

    create4

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -17,8 +17,11 @@ int create_file(const char *filename, char *text_content)
 
 	if (filename == NULL)
 		return (-1);
-	for (l = 0; text_content[l] != '\0'; l++)
+	if (text_content != NULL)
 	{
+		for (l = 0; text_content[l] != '\0'; l++)
+		{
+		}
 	}
 	f = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
 	if (f == -1)
