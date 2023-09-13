2de545a415c665a88e057dff8b0dba3d42225c81
Kelechiswe <kalleenz070@gmail.com>
2023-09-13 11:05:37 -0700

    copyy

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -36,7 +36,7 @@ int main(int argc, char *argv[])
 			exit(98);
 		}
 		w = write(file_to, buff, r);
-		if (file_to == -1 || w == 1)
+		if (file_to == -1 || w == -1)
 		{
 			dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
 			free(buff);
