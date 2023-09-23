46f78b18030a3b4ee93fe8608a8d61bb7fa43d52
Kelechiswe <kalleenz070@gmail.com>
2023-09-22 19:22:15 -0700

    elfs3

--- 0x15-file_io/100-elf_header.c
+++ 0x15-file_io/100-elf_header.c
@@ -123,7 +123,8 @@ void magic(unsigned char *e_ident)
  */
 void abi_v(unsigned char *e_ident)
 {
-	printf("  ABI Version:          %d\n", e_ident[EI_ABIVERSION]);
+	printf("  ABI Version:            %d\n",
+	e_ident[EI_ABIVERSION]);
 }
 
 /**
@@ -132,7 +133,8 @@ void abi_v(unsigned char *e_ident)
  */
 void class(unsigned char *e_ident)
 {
-	printf(" Class: ");
+	printf("  Class:            ");
+
 	switch (e_ident[EI_CLASS])
 	{
 		case ELFCLASS64:
@@ -155,7 +157,8 @@ void class(unsigned char *e_ident)
  */
 void data(unsigned char *e_ident)
 {
-	printf(" Data: ");
+	printf("  Data:            ");
+
 	switch (e_ident[EI_DATA])
 	{
 		case ELFDATANONE:
@@ -178,7 +181,8 @@ void data(unsigned char *e_ident)
  */
 void version(unsigned char *e_ident)
 {
-	printf(" Version: %d", e_ident[EI_VERSION]);
+	printf("  Version:            %d", e_ident[EI_VERSION]);
+
 	switch (e_ident[EI_VERSION])
 	{
 		case EV_CURRENT:
@@ -196,7 +200,8 @@ void version(unsigned char *e_ident)
  */
 void os_abi(unsigned char *e_ident)
 {
-	printf(" OS/ABI: ");
+	printf("  OS/ABI:            ");
+
 	switch (e_ident[EI_OSABI])
 	{
 		case ELFOSABI_NONE:
@@ -244,7 +249,8 @@ void type(unsigned int e_type, unsigned char *e_ident)
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 		e_type >>= 8;
 
-	printf(" Type: ");
+	printf("  Type:            ");
+
 	switch (e_type)
 	{
 		case ET_NONE:
@@ -274,7 +280,7 @@ void type(unsigned int e_type, unsigned char *e_ident)
  */
 void entry(unsigned long int e_entry, unsigned char *e_ident)
 {
-	printf(" Entry point address: ");
+	printf("  Entry point address:            ");
 
 	if (e_ident[EI_DATA] == ELFDATA2MSB)
 	{
