163675f667bce3dd540df5424620a9eb43b61b41
kelechiswe <kalleenz070@gmail.com>
2023-09-28 13:56:01 -0700

    end

---
+++ 0x14-bit_manipulation/100-get_endianness.c
@@ -0,0 +1,13 @@
+#include "main.h"
+
+/**
+ * get-endianness - checking the endianness
+ * Return: an integer
+ */
+int get_endianness(void)
+{
+	unsigned int i = 1;
+	char *e = (char *) &i;
+
+	return (*e);
+}
