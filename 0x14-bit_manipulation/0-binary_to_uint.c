e58bee66a7cb81b7ab5beaaaab416477a0f04f5a
kelechiswe <kalleenz070@gmail.com>
2023-09-28 12:48:08 -0700

    zero2

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -9,16 +9,18 @@
 unsigned int binary_to_uint(const char *b)
 {
 	unsigned int integer = 0;
+	int i;
 
 	if (b == NULL)
 		return (0);
 
-	while (*b != '\0')
+	i = 0;
+	while (b[i] != '\0')
 	{
-		if (*b != '0' || *b != '1')
+		if (b[i] != '0' || b[i] != '1')
 			return (0);
 
-		integer = 2 * integer + (*b - '0');
+		integer = 2 * integer + (b[i] - '0');
 		b++;
 	}
 	return (integer);
