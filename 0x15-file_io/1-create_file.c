906ddcc513f9c6a16274856ecb68c3779027b77f
kelechiswe <kalleenz070@gmail.com>
2023-10-02 14:12:45 -0700

    corrections

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -26,5 +26,5 @@ int create_file(const char *filename, char *text_content)
 	if (w == -1)
 		return (-1);
 	close(o);
-	return (len);
+	return (1);
 }
--- 0x15-file_io/2-append_text_to_file.c
+++ 0x15-file_io/2-append_text_to_file.c
@@ -26,5 +26,5 @@ int append_text_to_file(const char *filename, char *text_content)
 	if (w == -1)
 		return (-1);
 	close(o);
-	return (l);
+	return (1);
 }
